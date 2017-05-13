5887490692448a661bf76402dfaa05666b7072bd
OkYesAnap <OkYesAnap@gmail.com>
2017-05-13 15:53:33 +0300

    new files from PC

---
+++ Lesson1/src/main.js
@@ -0,0 +1,23 @@
+/*var name = 'Andrew';
+var age = 31;
+var itsTrue = true;
+const ZP = 100;
+
+console.log('Имя '+ name +'\nВозраст ='+ age +'\nЗарплата-'+ZP+'\nЭто-'+itsTrue);
+
+const user = {
+    name : 'Andrew',
+    age : 31,
+    itsTrue : true,
+    ZP : 100,
+}
+
+console.log(user);
+*/
+user1 = {
+   user2 : {
+    status : 'Easy CODE students'
+   }
+}
+console.log(user1.user2);
+console.log(user1.user2.status);
