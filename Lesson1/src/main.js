bbc0aea45db67facb0c7517055b71e17db1774c2
OkYesAnap <OkYesAnap@gmail.com>
2017-05-13 16:56:03 +0300

    homework1

--- Lesson1/src/main.js
+++ Lesson1/src/main.js
@@ -14,10 +14,25 @@ const user = {
 
 console.log(user);
 */
-user1 = {
-   user2 : {
-    status : 'Easy CODE students'
-   }
+user = {
+    contacts : {
+        phones : 
+        {
+            mobile : ['+380503032120', '+380500000000'],
+            home : '+3805777777'
+           
+        },
+        email : 'okyesanap@gmail.com'
+       
+    },
+    name : 
+    {
+        F : 'Panaseyko',
+        I : 'Andrew',
+        O : 'Vasilevich'
+    },
+    sexMale : true,
+    birthdate : '1986-02-28',
+    age : 31 
 }
-console.log(user1.user2);
-console.log(user1.user2.status);
+console.log(user);
