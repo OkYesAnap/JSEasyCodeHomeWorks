4d9f686b6da21b50b52c367e17a5f6206b2829fb
OkYesAnap <OkYesAnap@gmail.com>
2017-05-17 11:24:33 +0300

    Homework2

---
+++ Lesson2/src/main.js
@@ -0,0 +1,30 @@
+
+
+
+
+//const HelloString = "Дабрый День"
+
+//console.log(HelloString);
+
+/*var user = {
+    cars : [ 'Jiguli', 'BMW', 'Zaporojec'],
+    name : 'andew',
+
+}
+
+console.log ("у " + user.name + " есть" +' '+ user.cars[0] + ' ' + user.cars[1] + ' '  + user.cars[2]);
+console.log (`у ${user.name} есть ${user.cars[0]} ${user.cars[1]} ${user.cars[2]}`);
+
+
+  var name = '1234567890';
+
+  var myname  = name.slice(  2, -2 ).toUpperCase(); 
+  //+ name.slice(-1).toLowerCase();
+
+  console.log (myname);
+  */
+//Дестуруктуризация массива Math.min(...[50, 10, 20]);
+  const year = 2017.122;
+    
+    console.log (year.toFixed(5));
+
