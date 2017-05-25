a64886a812fadb5203aa02754f18b2e0bdb3a6d2
OkYesAnap <OkYesAnap@gmail.com>
2017-05-25 14:12:40 +0300

    Homework4

--- Lesson4/src/main.js
+++ Lesson4/src/main.js
@@ -1,58 +1,94 @@
+/*
+ 1. Напишите функцию которая принимает 2 числа 
+ и возвращает массив содержащий числа между первым числом и вторым числом;
+ */
+//первое и второе задание слепил в кучу
+ function numbersBetween(a, b) {
+     var returnArray =[];
+ 	 if (b == undefined){//если нет 2го параметра делам 2 задание:) 
+		for (var i = 0; i <= a; i++){
+        returnArray.push(fizzBuzz(i)); // вот тут вызывается функция для 2го задания
+	 }
+	 }
+	 else {
+	     a++; //это шоб не брало первое значание
+		for (var i = a; i < b; i++){
+        returnArray.push(i);
+     }
+	 }
+     return returnArray;
+ }
+
+function fizzBuzz(num) {// функция для второго задания
+  /* Ваш код здесь */
+  return (num === 0 ? '0 деленный на число будет 0' :
+          (((num % 3 == 0) && (num % 5 == 0)) ? 'FizzBuzz' : 
+            ((num % 5 == 0) ? 'Buzz' : 
+              (((num % 3 == 0) ? 'Fizz' :
+                num)))));
+	}
+ console.log(numbersBetween(1, 5)); // 2,3,4
+ console.log(numbersBetween(3, 6)); // 4,5
+ console.log(numbersBetween(12, 15)); // 13,14
+ console.log(numbersBetween(10, 15)); // 11, 12, 13, 14
+ console.log(numbersBetween(100)); //Для второго задания.
+ 
+/*
+ 2. Перепешите задачу FizzBuzz, но с использованием цикла. 
+ Расчет чисел должен идти до 100 
+ */
 
 
-// for ( var i = 1; i <= 1000; i++){
-//     console.log(i % 2 === 0 ? i : '');
-// }
+/*
+ 3. Напишите функцию которая принимает 1 аргумент - массив
+ И возвращает новый массив содержащий типы значений переменных
+ */
 
-// for ( var i = 0; i <= 1000; i+=2){
-//     console.log(i);
-// }
+let arr = [1, null, undefined, 'str', {}, [], function() {}];
 
-// function arrayFunc(arr){
-// for ( var i = 0; i < arr.length; i++)
-//      console.log(arr[i]*2);
-// }
-
-// arrayFunc([1,2,4,5,6,7,8,9,10]);
-
-
-
-// function arrayFunc(arr, x){
-// localArr = [];
-// for ( var i = 0; i < arr.length; i++){
-//     if (arr [i] % 2 === 1){
-//       localArr.push (arr [i]);
-//       x(i)
-//     }
-// }
-// return localArr;
-// }
-
-
-// var array = [11,20,31,40,51,60,71,80,91,10];
-// console.log(arrayFunc(array, function arrayDouble(x){
-//     console.log(`Нечетоное число номер ${x}`);
-// }
-
-// var array = [11,20,31,40,51,60,71,80,91,10];
-// var i =0;
-// while(Math.random() * 10 < 0){
-
-//     console.log();
-// }
-
-var project = {
-    name: '',
-    salary: '1200$',
-    technology: ['CSS3', 'HTML', 'MYSQL']
+function arrayTypes(arrayT){
+for (var i =0; i < arrayT.length; i++ ){
+arrayT[i] = typeof(arrayT[i]);
 }
+return(arrayT);
+} 
+console.log(arrayTypes(arr));
+/*
+ @@SUPER@@. Вам дан массив array, содержащий внутри объекты. 
+ Напишите функцию которая внутри цикла проходится по каждому элементу массива
+ И проверяет какой тип данных содержит свойство age, если тип данных NaN, 
+ тогда добавляет данному объекту свойство unknownAge: true
 
-for (let key in project){
-    if (Array.isArray(project[key]) == true) {      
-    console.log(Array.isArray(project[key]));
-        for (var i = 0; i < project[key].length; i++){
-            console.log(project[key][i]);
-        }
+ На основании нового массива, создайте новую функцию, которая вернет новый массив 
+  содержащий все объекты содержащие свойство unknownAge:true
+ */
 
+let array = Array.from({ length: 35 }).map(
+  (value, index) => (index % 2 ? { age: index + 2 } : { age: NaN })
+);
+
+//console.log(array); // [ {age:NaN}, {age:3}, {age:NaN}, {age:5}, {age:NaN}, {age:7} ....]
+//console.log(array.length); // 35
+
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++){
+    if (isNaN(arr[i].age)){
+        arr[i].unknownAge = true;
     }
+    }
+    return arr;
 }
+
+function unknownAge(arr){
+    let localArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i].unknownAge){
+            localArr.push(arr[i]);
+        }
+    }
+    return(localArr);
+}
+
+console.log(solution(array));
+console.log(unknownAge(solution(array)));
+
