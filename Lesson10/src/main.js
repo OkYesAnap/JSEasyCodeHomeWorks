2463cb3ebeed38c0214f376d0ae562ee650e50ed
OkYesAnap <OkYesAnap@gmail.com>
2017-06-16 18:31:08 +0300

    hw10

--- Lesson10/src/main.js
+++ Lesson10/src/main.js
@@ -80,37 +80,48 @@ ezjQuery
  */
 
 // example
-let tegs1 = function() {
-  let string = "";
-  tegs1.add = function(teg, add) {
-    ((string = string.slice(0, string.indexOf('></')+1) + $(teg, add) + string.slice(string.indexOf('></')+1, string.length)));
+let tegs = {
+  tegArr: { tBegin: [], tEnd: [] },
+  add: function(teg, content = "") {
+    this.tegArr.tBegin.push(`<${teg}>`);
+    this.tegArr.tBegin.push(`${content}`);
+    this.tegArr.tEnd.unshift(`</${teg}>`);
     return this;
-  };
-  tegs1.render = function(){
-    let ret = string;
-    string = '';
-    return ret;
+  },
+  render: function() {
+    let joinArr = this.tegArr.tBegin.concat(this.tegArr.tEnd);
+    this.tegArr = { tBegin: [], tEnd: [] };
+    return joinArr.join("");
   }
 };
 
 
-var ezjQuery1 = tegs1; 
-ezjQuery1();
-var helloList = ezjQuery1
-  .add('body') // <body></body>
-  .add('div') // <body><div></div></body>
-  .add('ul') // <body><div><ul></ul></div></body>
-  .add('li', 'Hello') //<body><div><ul><li>Hello</li></ul></div></body>
+var ezjQuery = tegs;
+var helloList = ezjQuery
+  .add("body") // <body></body>
+  .add("div") // <body><div></div></body>
+  .add("ul") // <body><div><ul></ul></div></body>
+  .add("li", "Hello") //<body><div><ul><li>Hello</li></ul></div></body>
   .render();
 console.log(helloList); // <body><div><ul><li>Hello</li></ul></div></body>
 //  Обратите внимание, что после вызова render создание строки началось сначала
+console.log(
+  ezjQuery
+    .add("table", "</привет")
+    .add("tr", "></ рыба")
+    .add("td", "></ мясо")
+    .render()
+);
+console.log(ezjQuery.add("div", "</ хвост").render());
+console.log(ezjQuery.add("div", "/ привет").render());
 
-var bodyDiv = ezjQuery1
-  .add('body') //<body></body>
-  .add('div') //<body><div></div></body>
+var bodyDiv = ezjQuery
+  .add("body") //<body></body>
+  .add("div") //<body><div></div></body>
   .render();
 console.log(bodyDiv); //<body><div></div></body>
 document.write(helloList);
+
 // Для выполнивших все задания
 // сделайте document.write(helloList) увидите результат :)
 
